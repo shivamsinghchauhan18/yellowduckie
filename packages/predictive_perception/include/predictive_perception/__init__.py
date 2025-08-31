786f3dc5871898b44efba24dbc778c85b48f4cad
shivamsinghchauhan18 <shivam.chauhan@tum.de>
2025-08-31 18:50:00 +0200

    init

--- packages/predictive_perception/include/predictive_perception/__init__.py
+++ packages/predictive_perception/include/predictive_perception/__init__.py
@@ -1,1 +1,19 @@
 # Predictive Perception Package
+
+# Import main modules to make them available
+from .multi_object_tracker import MultiObjectTracker
+from .kalman_tracker import KalmanTracker
+from .sensor_fusion import SensorFusion
+from .scene_analyzer import SceneAnalyzer
+from .motion_models import ConstantVelocityModel, ConstantAccelerationModel
+from .trajectory_visualizer import TrajectoryVisualizer
+
+__all__ = [
+    'MultiObjectTracker',
+    'KalmanTracker', 
+    'SensorFusion',
+    'SceneAnalyzer',
+    'ConstantVelocityModel',
+    'ConstantAccelerationModel',
+    'TrajectoryVisualizer'
+]
